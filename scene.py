df3daf554d84f23434e0d69b51ad037161452c35
bucho <steelwind666@gmail.com>
2015-07-21 20:00:03 +0900

    移動処理実装 modified:   scene.py

--- scene.py
+++ scene.py
@@ -35,6 +35,10 @@ class Position(object):
   def xy(self):
     return (self._x, self._y)
 
+  def __add__(self, other):
+    x, y = other.xy()
+    return Position(self._x + x, self._y + y)
+
 class PlayerCharacter(object):
   def __init__(self):
     self._point = Position(1, 1)
@@ -44,6 +48,20 @@ class PlayerCharacter(object):
     screen.set_color(Color.WHITE)
     screen.write('@')
     screen.move(self._point.xy())
+
+  def move(self, direction):
+    self._point += direction
+    return self
+
+class Direction(object):
+  EAST  = Position( 1,  0)
+  WEST  = Position(-1,  0)
+  NORTH = Position( 0, -1)
+  SOUTH = Position( 0,  1)
+  NORTH_EAST = NORTH + EAST
+  NORTH_WEST = NORTH + WEST
+  SOUTH_EAST = SOUTH + EAST
+  SOUTH_WEST = SOUTH + WEST
 
 class DungeonScene(Scene):
   def __init__(self):
@@ -61,6 +79,14 @@ class DungeonScene(Scene):
     if key == 'q':
       GameScene.change(EndingScene())
       return
+    elif key == 'l': self._player.move(Direction.EAST)
+    elif key == 'h': self._player.move(Direction.WEST)
+    elif key == 'k': self._player.move(Direction.NORTH)
+    elif key == 'j': self._player.move(Direction.SOUTH)
+    elif key == 'y': self._player.move(Direction.NORTH_WEST)
+    elif key == 'u': self._player.move(Direction.NORTH_EAST)
+    elif key == 'b': self._player.move(Direction.SOUTH_WEST)
+    elif key == 'n': self._player.move(Direction.SOUTH_EAST)
     self._screen.clear()
     self._player.draw(self._screen)
 
