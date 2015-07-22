1245d653ad875bac82e824239283f5a08c3b0bac
y-ogata <steelwind666@gmail.com>
2015-07-22 15:43:35 +0900

    ファイル分割 new file:   floor.py modified:   scene.py

---
+++ floor.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+class Position(object):
+  def __init__(self, x, y):
+    self._x, self._y = (x, y)
+
+  def __add__(self, other):
+    x, y = other.xy()
+    return Position(self._x + x, self._y + y)
+
+  def xy(self):
+    return (self._x, self._y)
+
+  def around(self):
+    x, y = self._x, self._y
+    return (Position(x-1, y-1), Position(x,   y-1), Position(x+1, y-1),
+            Position(x-1, y  ),                     Position(x+1, y  ),
+            Position(x-1, y+1), Position(x  , y+1), Position(x+1, y+1))
+
+class Direction(object):
+  EAST  = Position( 1,  0)
+  WEST  = Position(-1,  0)
+  NORTH = Position( 0, -1)
+  SOUTH = Position( 0,  1)
+  NORTH_EAST = NORTH + EAST
+  NORTH_WEST = NORTH + WEST
+  SOUTH_EAST = SOUTH + EAST
+  SOUTH_WEST = SOUTH + WEST
+
+MAP = ( [
+      list('##########################'),
+      list('#....######.........######'),
+      list('#....####.+.#.....#.######'),
+      list('##+####...#.###.###.+...##'),
+      list('##...>..###.#.....#.###.##'),
+      list('###########.........#>..##'),
+      list('##########################'),
+      ],
+      [
+      list('##########################'),
+      list('###########...#.#...+..###'),
+      list('#########.+.#.#...#.##.###'),
+      list('#######...#.#.#..##.#...##'),
+      list('###.....###.#.....#.#...##'),
+      list('#>..#######.###.....#<..##'),
+      list('##########################'),
+      ])
+
+class CurrentFloor(object):
+  _floor = 0
+  _terrain = MAP[_floor]
+
+  def terrain_at(self, position):
+    x, y = position.xy()
+    return CurrentFloor._terrain[y][x]
+
+  def draw_at(self, position, screen):
+    screen.move(position.xy())
+    screen.write(self.terrain_at(position))
+
+  def is_last_floor(self):
+    return CurrentFloor._floor == len(MAP) - 1
+
+  def can_walk(self, position):
+    return self.terrain_at(position) != '#'
+
+  def is_down_stairs_at(self, position):
+    return self.terrain_at(position) == '>'
+
+  def next(self):
+    CurrentFloor._floor += 1
+    CurrentFloor._terrain = MAP[CurrentFloor._floor]
--- scene.py
+++ scene.py
@@ -3,6 +3,9 @@ from app import Screen
 from app import Color
 from game import Game
 from game import GameScene
+from floor import CurrentFloor 
+from floor import Position
+from floor import Direction
 
 class Scene(object):
   def finalize(self):
@@ -28,33 +31,6 @@ class TitleScene(Scene):
     key = self._screen.read_key()
     GameScene.change(DungeonScene())
 
-class Position(object):
-  def __init__(self, x, y):
-    self._x, self._y = (x, y)
-
-  def __add__(self, other):
-    x, y = other.xy()
-    return Position(self._x + x, self._y + y)
-
-  def xy(self):
-    return (self._x, self._y)
-
-  def around(self):
-    x, y = self._x, self._y
-    return (Position(x-1, y-1), Position(x,   y-1), Position(x+1, y-1),
-            Position(x-1, y  ),                     Position(x+1, y  ),
-            Position(x-1, y+1), Position(x  , y+1), Position(x+1, y+1))
-
-class Direction(object):
-  EAST  = Position( 1,  0)
-  WEST  = Position(-1,  0)
-  NORTH = Position( 0, -1)
-  SOUTH = Position( 0,  1)
-  NORTH_EAST = NORTH + EAST
-  NORTH_WEST = NORTH + WEST
-  SOUTH_EAST = SOUTH + EAST
-  SOUTH_WEST = SOUTH + WEST
-
 class PlayerCharacter(object):
   def __init__(self):
     self._position = Position(1, 1)
@@ -77,51 +53,7 @@ class PlayerCharacter(object):
 
   def moved_position(self, direction):
     return self._position + direction
-
-MAP = ( [
-      list('##########################'),
-      list('#....######.........######'),
-      list('#....####.+.#.....#.######'),
-      list('##+####...#.###.###.+...##'),
-      list('##...>..###.#.....#.###.##'),
-      list('###########.........#>..##'),
-      list('##########################'),
-      ],
-      [
-      list('##########################'),
-      list('###########...#.#...+..###'),
-      list('#########.+.#.#...#.##.###'),
-      list('#######...#.#.#..##.#...##'),
-      list('###.....###.#.....#.#...##'),
-      list('#>..#######.###.....#<..##'),
-      list('##########################'),
-      ])
-
-class CurrentFloor(object):
-  _floor = 0
-  _terrain = MAP[_floor]
-
-  def terrain_at(self, position):
-    x, y = position.xy()
-    return CurrentFloor._terrain[y][x]
-
-  def draw_at(self, position, screen):
-    screen.move(position.xy())
-    screen.write(self.terrain_at(position))
-
-  def is_last_floor(self):
-    return CurrentFloor._floor == len(MAP) - 1
-
-  def can_walk(self, position):
-    return self.terrain_at(position) != '#'
-
-  def is_down_stairs_at(self, position):
-    return self.terrain_at(position) == '>'
-
-  def next(self):
-    CurrentFloor._floor += 1
-    CurrentFloor._terrain = MAP[CurrentFloor._floor]
-    
+   
 class DungeonScene(Scene):
   def __init__(self):
     Scene.__init__(self)
